23048723899b6aa3a078048fab5f9def54bd5357
dushenko1992 <dushenko1992@gmail.com>
2020-08-16 15:39:29 +0300

    add header

---
+++ app/blocks/header/header.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  //burger
+  if (document.querySelector(".js-burger")) {
+    $(".js-burger").click(function () {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(".js-nav").removeClass("active");
+        $("body").removeClass("overflow");
+      } else {
+        $(this).addClass("active");
+        $(".js-nav").addClass("active");
+        $("body").addClass("overflow");
+      }
+    });
+  }
+
+  //scroll to section
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".header").addClass("active");
+    } else {
+      $(".header").removeClass("active");
+    }
+  });
+});
--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,28 @@
 "use strict";
 
-//=require ../blocks/**/*.js
+$(document).ready(function () {
+  //burger
+  if (document.querySelector(".js-burger")) {
+    $(".js-burger").click(function () {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(".js-nav").removeClass("active");
+        $("body").removeClass("overflow");
+      } else {
+        $(this).addClass("active");
+        $(".js-nav").addClass("active");
+        $("body").addClass("overflow");
+      }
+    });
+  } //scroll to section
+
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".header").addClass("active");
+    } else {
+      $(".header").removeClass("active");
+    }
+  });
+});
 $(document).ready(function () {});
