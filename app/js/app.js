8acd8dde4c587b37225f6addb0d69c40b21e9460
dushenko1992 <dushenko1992@gmail.com>
2020-08-17 21:15:19 +0300

    add about slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,14 @@
 //=require ../blocks/**/*.js
 $(document).ready(function () {
-  
+  var swiperIntro = new Swiper(".js-about-slider", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".js-about-next",
+      prevEl: ".js-about-prev",
+    },
+  });
 });
--- build/js/app.js
+++ build/js/app.js
@@ -49,4 +49,16 @@ $(document).ready(function () {
     return false;
   });
 });
-$(document).ready(function () {});
+$(document).ready(function () {
+  var swiperIntro = new Swiper(".js-about-slider", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".js-about-next",
+      prevEl: ".js-about-prev"
+    }
+  });
+});
