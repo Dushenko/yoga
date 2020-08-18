ca863662e6bd6c194ad3bcc3edf797f63b5a5712
dushenko1992 <dushenko1992@gmail.com>
2020-08-18 03:52:01 +0300

    add form validation

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,23 @@
 //=require ../blocks/**/*.js
 $(document).ready(function () {
-  //nice select
-	$("select").niceSelect();
-	
-	//prise hover
-	$(".price__btn").hover(function () {
+
+  //prise hover
+  $(".price__btn").hover(function () {
     $(this).parent().parent().find(".price__head").toggleClass("active");
+  });
+
+  //validation form
+  $("#formClass").validate({
+		ignore: [],
+    rules: {
+      user_name: { required: true },
+      user_phone: { required: true },
+      select_class: { required: true },
+    },
+    messages: {
+      user_name: "Please enter your name",
+      user_phone: "Please enter your phone number",
+      select_class: "Please select a class",
+    },
   });
 });
